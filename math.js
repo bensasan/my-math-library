da2e90ec026d49e916e3ef6ef33b2a7b69019318
bensasan <150501278+bensasan@users.noreply.github.com>
2025-06-25 09:18:13 +0700

    Add basic calculator functions and CLI

--- math.js
+++ math.js
@@ -6,5 +6,23 @@ function sum(a, b) {
   }
 }
 
+function add(a, b) {
+  return a + b
+}
 
-module.exports={sum};
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero')
+  }
+  return a / b
+}
+
+module.exports = { sum, add, subtract, multiply, divide }
--- math.test.js
+++ math.test.js
@@ -3,3 +3,9 @@ const assert = require('assert')
 
 assert.strictEqual(math.sum(2, 2), 4, 'should sum two numbers')
 assert.strictEqual(math.sum([1, 2, 3, 4, 5]), 15, 'should sum an array')
+
+assert.strictEqual(math.add(3, 4), 7, 'add should work')
+assert.strictEqual(math.subtract(5, 2), 3, 'subtract should work')
+assert.strictEqual(math.multiply(3, 4), 12, 'multiply should work')
+assert.strictEqual(math.divide(10, 2), 5, 'divide should work')
+assert.throws(() => math.divide(1, 0), /Cannot divide by zero/, 'divide by zero')
